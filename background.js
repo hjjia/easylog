322fd33e40e0c9147c7a9b8a4c676e1000f81d92
mingazi <mingazi@163.com>
2019-05-26 17:41:26 +0800

    listen all url

--- background.js
+++ background.js
@@ -5,10 +5,31 @@
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {hostEquals: 'www.baidu.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
   });
+
+
+
+// https://blog.csdn.net/hhmouse111/article/details/36901527
+chrome.webRequest.onBeforeRequest.addListener (
+ 
+    function(details) {
+    
+        chrome.tabs.query({active:true},function(tab){
+            // 当前页面的url
+            var pageUrl = tab[0].url;
+            // 在这可以写判断逻辑，将请求cancel掉，或者将请求打印出来
+            console.log("current url -> " + pageUrl);
+        });
+ 
+    },
+     
+    {urls:["*://*/*"]},  //监听页面请求,你也可以通过*来匹配。
+    ["blocking"] 
+);
+
