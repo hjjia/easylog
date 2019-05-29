e5b00c6f4c4dc3734242cd94d0a6b95fdab389c1
mingazi <mingazi@163.com>
2019-05-30 00:34:56 +0800

    page action

--- popup.js
+++ popup.js
@@ -6,11 +6,16 @@
   });
 
 
+console.log("popup");
+
 // 相当于点击了插件的icon
 document.addEventListener('DOMContentLoaded', function () {
     var data = chrome.extension.getBackgroundPage().Data;
     var ajaxdata = chrome.extension.getBackgroundPage().ajaxData;
 
-    $("#result").text(data);
-    $("#ajaxresult").text(ajaxdata);
+	console.log('log data',data);
+	console.log('ajax data',ajaxdata);
+        //data: JSON.stringify(Data),
+    $(".result").text(JSON.stringify(data));
+    $(".jaxresult").text(JSON.stringify(ajaxdata));
 });
